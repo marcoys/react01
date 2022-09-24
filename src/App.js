aee0433162ebd297bc0ab1c336d0680b9e3f320a
Marco <mmmarcoys@gmail.com>
2022-09-24 20:27:40 +0900

    220924 mac

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable  */
+
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
@@ -5,15 +7,26 @@ import { useState } from 'react';
 function App() {
 
   let post = '강남 우동 맛집';
-  let [글제목, a] = useState(['남자 코트 추천','강남 우동맛집', '파이썬독학']);
+  let [글제목, 첫째변경] = useState(['남자 코트 추천','강남 우동맛집', '파이썬독학']);
+  let [따봉, 따봉변경] = useState(0);
+
+  function firChange() {
+    let newArr = [...글제목];
+
+    newArr[0] = '여자 코트 추천';
+    첫째변경(newArr);
+  }
 
   return (
     <div className="App">
       <div className="black-nav">
         <h4>ReactLogo</h4>
       </div>
+      <button>가나다 순 정렬</button>
+      <br />
+      <span onClick={firChange}>바꾸기</span>
       <div className="list">
-        <h4>{ 글제목[0] }</h4>
+        <h4>{ 글제목[0] } <span onClick={() => { 따봉변경(따봉+1)} }>👍</span> {따봉} </h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
