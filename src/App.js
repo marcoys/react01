3bc1f02698fb2d4161af252f79ea7e682d3f04b2
Marco <mmmarcoys@gmail.com>
2022-09-28 17:41:30 +0900

    220928 mac

--- src/App.js
+++ src/App.js
@@ -6,10 +6,17 @@ import { useState } from 'react';
 
 function App() {
 
+  let today = new Date();
+  let month = today.getMonth() + 1;
+  let date = today.getDate();
+
   let post = '강남 우동 맛집';
   let [글제목, 글제목변경] = useState(['남자 코트 추천','강남 우동맛집', '파이썬독학']);
-  let [따봉, 따봉변경] = useState(0);
+  let [따봉, 따봉변경] = useState([0, 1, 0]);
   let [modal, setModal] = useState(false);
+  let [title, setTitle] = useState(0);
+  let [입력값, 입력값변경] = useState('');
+  let [삭제, 삭제변경] = useState(0);
 
   function firChange() {
     let newArr = [...글제목];
@@ -25,12 +32,32 @@ function App() {
     글제목변경(gndArr);
   }
 
-  function modalToggle() {
-    if (modal == false) {
-      setModal(true);
-    } else {
-      setModal(false);
+  function tUP(i) {
+    let upArr = [...따봉];
+
+    upArr[i] = upArr[i] + 1;
+    따봉변경(upArr);
+  }
+
+  function addTxt() {
+    let titVal = document.querySelector('#iptit').value;
+    let addArr = [...글제목];
+    if ( titVal == '' ) {
+      alert('제목을 입력해주세여')
+      return false;
     }
+
+    addArr.unshift(입력값);
+    console.log(글제목변경(addArr));
+
+    titVal = '';
+  }
+
+  function delTxt(n) {
+    let delArr = [...글제목];
+
+    delArr.splice(n,1);
+    글제목변경(delArr);
   }
 
   return (
@@ -68,34 +95,36 @@ function App() {
         글제목.map(function(a, i) {
           return (
             <div className="list" key={i}>
-              <h4 onClick={() => {setModal(!modal)}}>{글제목[i]}<span
-            onClick={() => {
-              따봉변경(따봉 + 1);
-            }}
-          >
-            👍
-          </span>{' '}
-          {따봉}{' '}</h4>
+              <h4 onClick={() => {setModal(!modal); setTitle(i)}}>{글제목[i]}<span
+            onClick={(e) => { e.stopPropagation(); tUP(i) }}>👍</span>{' '}
+            {따봉[i]}{' '}</h4>
               <p>2월 17일 발행</p>
+              <button onClick={() => {delTxt(i)}}>삭제</button>
             </div>
           )
         })
       }
 
+      <input id='iptit' onChange={(e) => {입력값변경(e.target.value);
+      console.log(입력값)}}/>
+      <button onClick={addTxt}>글발행</button>
+
       {
         //조건식 ? 참일 떄 실행되는 코드 : 거짓일때 실행되는 코드 --> 삼항연산자
-        modal == true ? <Modal></Modal> : null
+        modal == true ? <Modal 글제목={글제목} 글수정={firChange} title={title}></Modal> : null
       }
     </div>
   );
+  
 }
 
-function Modal() {
+function Modal(props) {
   return (
     <div className="modal">
-      <h4>제목</h4>
+      <h4>{props.글제목[props.title]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
+      <button onClick={props.글수정}>글수정</button>
     </div>
   );
 }
